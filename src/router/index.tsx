6b37fbf4605560ea25bfdb4304de4d25de042260
daniziadulmarwan <zeronosvegaaltair@gmail.com>
2024-04-29 14:46:25 +0700

    remove some trash

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,8 +3,13 @@ import ForgotPassword from "@/pages/ForgotPassword";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
 import ResetPassword from "@/pages/ResetPassword";
+import { useContext } from "react";
+import { AuthContext } from "@/contexts/auth";
 
 export default function Router() {
+  const token = useContext(AuthContext);
+  console.log(token);
+
   return (
     <Routes>
       <Route path="/" element={<Login />} />
